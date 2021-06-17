feab23e747392dcf8207b98337dff6799a463ae0
DESKTOP-CTVMTKB <firebase732@gmail.com>
2021-06-17 10:28:51 -0700

    layouts

---
+++ app/src/main/java/com/example/fypapp/Entertainmentmoduel/cartoons/fifthcartoon.java
@@ -0,0 +1,16 @@
+package com.example.fypapp.Entertainmentmoduel.cartoons;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fypapp.R;
+
+public class fifthcartoon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifthcartoon);
+    }
+}
