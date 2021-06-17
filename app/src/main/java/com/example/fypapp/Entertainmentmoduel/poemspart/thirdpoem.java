8d66719875ce464f4cc15f041321be8c617078bc
DESKTOP-CTVMTKB <firebase732@gmail.com>
2021-06-17 10:00:45 -0700

    adding poemlayouts

---
+++ app/src/main/java/com/example/fypapp/Entertainmentmoduel/poemspart/thirdpoem.java
@@ -0,0 +1,16 @@
+package com.example.fypapp.Entertainmentmoduel.poemspart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fypapp.R;
+
+public class thirdpoem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thirdpoem);
+    }
+}
