6e66dbdf00b8deff7adcad00c316cfd56d36a36f
Dell <ikramaelahi1@gmail.com>
2021-06-19 12:01:53 +0500

    teacherPanel

---
+++ app/src/main/java/com/example/fypapp/teacherpanel/teacherActivity.java
@@ -0,0 +1,16 @@
+package com.example.fypapp.teacherpanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fypapp.R;
+
+public class teacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher);
+    }
+}
