8ccc9baac056abaed787ab3b021ba1b62a80227a
DESKTOP-CTVMTKB <firebase732@gmail.com>
2021-07-09 03:09:41 -0700

    QuizScreen

---
+++ app/src/main/java/com/example/fypapp/Quiz/QuizMainActivity.java
@@ -0,0 +1,18 @@
+
+
+package com.example.fypapp.Quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fypapp.R;
+
+public class QuizMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_main);
+    }
+}
